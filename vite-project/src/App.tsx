729bea1ee2b61c03ac79f5493ea3063d6e6ff2cf
enascentedev <emanuelalvorada@gmail.com>
2025-03-01 23:11:34 -0300

    Feature: criando layout página de login

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,78 +1,11 @@
-import './index.css'
-import { Button } from "@/components/ui/button"
-import { Progress } from "@/components/ui/progress"
-import { Input } from "@/components/ui/input"
-
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
-import {
-  InputOTP,
-  InputOTPGroup,
-  InputOTPSeparator,
-  InputOTPSlot,
-} from "@/components/ui/input-otp"
-
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-
-
-
-function App() {
-  
-  return (
-    <div>
-		<h1 className='bg-slate-500 w-full h-full p-10 m-5 text-gray-900'>Projeto teste</h1>
-			<Button>enviar</Button>
-			<Progress value={33} />
-			<Popover>
-				<PopoverTrigger>abrir</PopoverTrigger>
-				<PopoverContent>Place content for the popover here.</PopoverContent>
-				<Input />
-
-			</Popover>
-			<InputOTP maxLength={6}>
-				<InputOTPGroup>
-					<InputOTPSlot index={0} />
-					<InputOTPSlot index={1} />
-					<InputOTPSlot index={2} />
-				</InputOTPGroup>
-				<InputOTPSeparator />
-				<InputOTPGroup>
-					<InputOTPSlot index={3} />
-					<InputOTPSlot index={4} />
-					<InputOTPSlot index={5} />
-				</InputOTPGroup>
-			</InputOTP>
-			<AlertDialog>
-				<AlertDialogTrigger>Open</AlertDialogTrigger>
-				<AlertDialogContent>
-					<AlertDialogHeader>
-						<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-						<AlertDialogDescription>
-							This action cannot be undone. This will permanently delete your account
-							and remove your data from our servers.
-						</AlertDialogDescription>
-					</AlertDialogHeader>
-					<AlertDialogFooter>
-						<AlertDialogCancel>Cancel</AlertDialogCancel>
-						<AlertDialogAction>Continue</AlertDialogAction>
-					</AlertDialogFooter>
-				</AlertDialogContent>
-			</AlertDialog>
+import React from 'react'
+import { Button } from "./components/ui/button"
+export function App() {
+	
+	return (
+		<div>
+			<Button>Click me</Button>
 		</div>
-  )
+	)
 }
 
-export default App
